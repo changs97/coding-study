61c72231f11240ee901182e69f86774b74803bfb
changs97 <86148926+changs97@users.noreply.github.com>
2024-01-04 15:48:06 +0900

    [Bronze V] Title: 사칙연산, Time: 124 ms, Memory: 13248 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.kt
@@ -0,0 +1,14 @@
+import java.util.Scanner
+
+fun main() {
+    val s = Scanner(System.`in`)
+    val a = s.nextInt()
+    val b = s.nextInt()
+
+
+    println(a + b)
+    println(a - b)
+    println(a * b)
+    println(a / b)
+    println(a % b)
+}
