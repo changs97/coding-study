d9982fa63208161d13953512c627fd26485c0ea9
changs97 <86148926+changs97@users.noreply.github.com>
2024-01-07 01:27:40 +0900

    [Bronze V] Title: A+B - 7, Time: 144 ms, Memory: 21764 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val n = readln().toInt()
+
+    System.`in`.bufferedReader().use { br ->
+        System.out.bufferedWriter().use { bw ->
+            for (i in 1..n) {
+                val (a, b) = br.readLine().split(" ").map { it.toInt() }
+                bw.write("Case #$i: ${a + b}\n")
+            }
+
+            bw.flush()
+        }
+    }
+}
