88483e9b238abd9c273f3de1f2cfe2ec8d9af61a
changs97 <86148926+changs97@users.noreply.github.com>
2024-01-20 16:06:26 +0900

    [Bronze I] Title: 소인수분해, Time: 128 ms, Memory: 12540 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.kt
+++ 백준/Bronze/11653. 소인수분해/소인수분해.kt
@@ -1,35 +1,18 @@
 fun main() {
-    var n = readln().toInt()
+    var n = readLine()!!.toInt()
+    val sb = StringBuilder()
 
-    val list1 = mutableListOf<Int>()
-
-    for (i in 2..n) {
-        if (n % i != 0) continue
-
-        val list2 = mutableListOf<Int>()
-
-        for (j in 2 until i) {
-            if (i % j == 0) {
-                list2.add(j)
-            }
-        }
-
-        if (list2.size == 0) list1.add(i)
-    }
-
-    val list3 = mutableListOf<Int>()
-
-    for (i in list1) {
-        if (list1.contains(n)) {
-            println(n)
-            return
-        }
-
-        if (n % i != 0) continue
-
-        while (n % i == 0) {
-            n /= i
-            println(i)
+    var cnt = 2
+    while (n > 1) {
+        // 나누어 떨어질 때
+        if (n % cnt == 0) {
+            sb.append(cnt).append("\n")
+            n /= cnt
+        } else {
+            // 나누어 떨어지지 않을 때
+            cnt++
         }
     }
+
+    print(sb)
 }
