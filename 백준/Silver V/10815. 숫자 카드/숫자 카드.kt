1d472ca4cda31f42921ea4729cc07f916f350a6f
changs97 <86148926+changs97@users.noreply.github.com>
2024-02-14 16:28:22 +0800

    [Silver V] Title: 숫자 카드, Time: 1260 ms, Memory: 186260 KB -BaekjoonHub

---
+++ 백준/Silver V/10815. 숫자 카드/숫자 카드.kt
@@ -0,0 +1,20 @@
+
+fun main() {
+    val bw = System.out.bufferedWriter()
+    val n = readln().toInt()
+    val myNumCards = readln().split(" ").map { it.toInt() }.toHashSet()
+
+    val m = readln().toInt()
+    val numCards = readln().split(" ").map { it.toInt() }
+
+    for (i in numCards) {
+        if (myNumCards.contains(i)) {
+            bw.write("1 ")
+        } else {
+            bw.write("0 ")
+        }
+    }
+
+    bw.flush()
+    bw.close()
+}
