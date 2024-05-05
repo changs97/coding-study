bab306a1bc675825c0e1a8bd40f20dcf65038e4d
이창형 <pck4949@naver.com>
2024-05-05 18:42:33 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 10.98 ms, Memory: 60.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.kt
@@ -0,0 +1,12 @@
+class Solution {
+    fun solution(s: String): String {
+        var answer = ""
+        answer = if (s.length % 2 == 0) {
+            s[s.length/2-1].toString() + s[s.length/2].toString()
+        } else {
+            s[s.length/2].toString()
+        }
+
+        return answer
+    }
+}
